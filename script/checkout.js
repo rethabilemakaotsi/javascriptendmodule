80edf38d20adb9ef4eddad79125798581717b526
rethabile <rethabilemakaotsi17@gmail.com>
2023-12-08 16:27:04 +0200

    uptade

---
+++ script/checkout.js
@@ -0,0 +1,45 @@
+let products = JSON.parse(localStorage.getItem('shoppingCart')) || []
+let output= document.querySelector('.output');
+
+let groupProducts = Object.groupBy(products, (id) => {
+    return id.id
+})
+let amount = []
+function getProduct(){
+
+    let totalPrice = 0;
+    let quantity = 0;
+output.innerHTML = ""
+    console.log(groupProducts.id);
+    for (let key in groupProducts) {
+
+        groupProducts[key].forEach(item => {
+            // totalPrice += item.price * ;
+            quantity += item.quantity;
+        });
+        amount.push(totalPrice)
+        
+        console.log(groupProducts);
+        output.innerHTML +=
+        `
+        <tr>
+            <td>${groupProducts[key][0].title}</td>
+            <td>${groupProducts[key][0].price}</td>
+            <td>${groupProducts[key].length}</td>
+
+        </tr>
+        `
+    }
+
+}
+getProduct(products)
+
+function clearProduct(value) {
+  
+      output.innerHTML = "";
+    localStorage.removeItem("shoppingCart");
+    
+
+}
+
+document.querySelector('.clearBtn').addEventListener('click', clearProduct);
--- script/product.js
+++ script/product.js
@@ -1,71 +1,99 @@
-let product = [
-    {
-        id: 0,
-        image: "https://i.postimg.cc/4NdVxjwd/product-2-removebg-preview.png",
-        
-        title: 'Jagermister',
-        price: 120,
-    },
+
+// created an array of objects
+
+let product = JSON.parse(localStorage.getItem('Product')) || [
     {
         id: 1,
-        image: "https://i.postimg.cc/W3kYChFB/HENNESSY-removebg-preview.png",
-        title: 'Ballianto',
-        price: 120,
+        image: "https://i.postimg.cc/4NdVxjwd/product-2-removebg-preview.png",
+        title: 'Jagermister',
+        price: 370,
     },
     {
         id: 2,
-        image: "https://i.postimg.cc/DzWx48gS/product3-removebg-preview.png",
+        image: "https://i.postimg.cc/W3kYChFB/HENNESSY-removebg-preview.png",
         title: 'Ballianto',
-        price: 120,
+        price: 500,
     },
     {
         id: 3,
-        image: "https://i.postimg.cc/DzjKnJzm/product-1-removebg-preview.png",
-        title: 'Donjlio',
-        price: 120,
+        image: "https://i.postimg.cc/DzWx48gS/product3-removebg-preview.png",
+        title: 'Ballianto',
+        price: 450,
     },
     {
         id: 4,
+        image: "https://i.postimg.cc/DzjKnJzm/product-1-removebg-preview.png",
+        title: 'Donjlio',
+        price: 700,
+    },
+    {
+        id: 5,
         image: "https://i.postimg.cc/W38q27kg/product4-removebg-preview.png",
         title: 'Azul',
-        price: 120,
+        price: 950,
     }
 ];
-
-document.getElementById('root').innerHTML = product.map((item, i) => {
-    let { image, title, price } = item;
-    return (
-    
-        `  <div class="col d-flex ">
-          <div class="card "" style="width: 18rem;" >
-        <img src="${image}" class="card-img-top w-100" alt="..." >
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class="card-text text-dark">R ${price}</p>
-            <button onclick='addtocart(${i})'>Add to cart</button>
-        </div>
-        </div>`
-
-    );
-
-}).join('');
-
+// item store at local storage and the search function
 localStorage.setItem('Product', JSON.stringify(product));
-let cart = [];
+let productWrapper = document.querySelector('#root');
+let searchInput = document.querySelector('[product-search]')
+function displayItems(items) {
+    productWrapper.innerHTML = ""
+    try{
+        if(items) {
+            // console.log(items);
+            items.forEach( item =>{
+                productWrapper.innerHTML +=`
+                    <div class="card" >
+                    <img src="${item.image}" class="card-img-top w-100" alt="${item.title}" >
+                    <div class="card-body">
+                        <h5 class="card-title">${item.title}</h5>
+                        <p class="card-text text-dark">R ${item.price}</p>
+                        <button onclick='addToCart(${JSON.stringify(item)})'>Add to cart</button>
+                    </div>
+                ` 
+            })
+        }
+    }catch(e) {
 
-function addtocart(a) {
-    cart.push({ ...product[a] });
-    displaycart();
+    }
 }
+displayItems(product)
+
+searchInput.addEventListener('keyup', ()=>{
+    try{
+        let itemFound = product.filter(item=>{
+            return item.title.toLowerCase().includes(searchInput.value.toLowerCase())
+        })
+        if(itemFound) {
+            displayItems(itemFound)
+        }else {
+            productWrapper.innerHTML = "Loading"
+        }
+    }catch(e) {
+
+    }
+})
+
+
+let cart = JSON.parse(localStorage.getItem('shoppingCart')) || [];
 
 function delElement(a) {
     cart.splice(a, 1);
     displaycart();
 }
 
+let addToCArt = []
+function addToCart(item) {
+    if(item){
+        cart.push(item);
+        localStorage.setItem('shoppingCart', JSON.stringify(cart))
+    }
+ }
+
 function displaycart() {
     let j = 0, total = 0;
-    document.getElementById("count").innerHTML = cart.length;
+    // document.getElementById("count").innerHTML = cart.length;
 
     if (cart.length == 0) {
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
@@ -89,10 +117,26 @@ function displaycart() {
     }
 }
 displaycart()
+//function for my sort butt
 
-
-
-function sortProductsByPrice() {
-    // Implement your logic for sorting products by price
-    console.log("Sort by Price button clicked");
-}
+function sortitems() {
+    let sortedarray = product.sort((data1, data2) => {
+      console.log(data1, data2);
+      return item1.price - item2.price;
+    });
+  
+    productWrapper.innerHTML = "";
+    sortedarray.forEach((item) => {
+      productWrapper.innerHTML += `
+       <div class="card" style="width: 15rem; height:20rem;">
+              <img src="${item.image}" class="card-img-top" alt="products" loading=lazy >
+              <div class="card-body">
+                  <h5 class="card-title">${item.title}</h5>
+                  <p class="card-text">R${item.price}</p>
+                  <button href="#" class="btn btn-secondary" onclick='addToCart(${JSON.stringify(
+                    item
+                  )})'>Add to Cart</button></div>`;
+    });
+  }
+  
+  document.getElementById('sortByPrice').addEventListener("click", sortitems);
